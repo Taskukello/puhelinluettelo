5a1bdc8847f51624d819ca31619e707feff5ff82
taskukello <ojantola@gmail.com>
2018-02-11 19:00:21 +0200

    git hienosaatoa

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
